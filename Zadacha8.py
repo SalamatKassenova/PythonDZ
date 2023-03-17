931e05459253b5cc6d6a2056b45261c12299baf4
Salamat Kassenova <kassenova1993@gmail.com>
2023-03-17 10:18:34 +0600

    more conditions added

--- Zadacha8.py
+++ Zadacha8.py
@@ -12,8 +12,14 @@ else:
     i=m
     x=n
 sum = int(n*m)
+if k == sum:
+    flag = False
+    print('у вас и так столько долек, разламывать не надо')  
+elif k>sum:
+    flag = False
+    print('в вашем шоколаде нет столько долек, купите еще шоколада')  
 while flag:
-    if (k%i == 0 and k>=i) or (k%x == 0 and k>=x):
+    if (k<sum and k%i == 0 and k>=i) or (k<sum and k%x == 0 and k>=x) :
         flag = False
         print('yes')
     else:
